b1a61de57154454e56457e02441513b49f7d21cb
sara <sara.alzadjali14@outlook.com>
2023-12-11 16:42:33 +0400

    Add project files.

---
+++ database/MyDbContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace database.MyDbContext
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        // initialize a connection inside the database
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // connection string
+
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-853KEKM\\MSSQLS;Initial Catalog=MakeenCompany;Integrated Security=True;");
+        }
+
+        public DbSet <Employee> Employees { get; set; }
+    }
+}
---
+++ database/Program.cs
@@ -0,0 +1,13 @@
+﻿using database.MyDbContext;
+
+namespace database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            
+        }
+    }
+}
